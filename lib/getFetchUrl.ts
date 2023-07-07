82cb1d2262a349c5c2205f5acb9faeefe10c3441
payalthakkar08 <payal.harwani3333@gmail.com>
2023-07-07 17:18:49 +0530

    Remove unnecessary code

--- lib/getFetchUrl.ts
+++ lib/getFetchUrl.ts
@@ -1,6 +1,2 @@
 export const getFetchUrl = (route: string) =>
-  `${
-    process.env.NODE_ENV === 'production'
-      ? `https://${process.env.VERCEL_URL!}`
-      : 'http://localhost:3000'
-  }/${route}`;
+  `${`https://${process.env.VERCEL_URL!}`}/${route}`;
